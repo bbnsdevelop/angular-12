274a7a8a51d03901ec7928c65d9528c4e190ad68
bbnsdevelop <bbnsdevelop@gmail.com>
2021-09-08 15:31:55 -0300

    creating index to custom imports

--- projects/calculator/src/app/app.module.ts
+++ projects/calculator/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CalculatorModule } from './calculator/calculator.module';
+import { CalculatorModule } from './calculator';
 
 @NgModule({
   declarations: [
