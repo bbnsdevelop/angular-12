355e5f4fbcdf3c227528dd43ddcd6aa9300f8628
bbnsdevelop <bbnsdevelop@gmail.com>
2021-09-08 15:36:47 -0300

    create component cal --> ng g c calculator/calc --module=calculator

--- projects/calculator/src/app/calculator/calculator.module.ts
+++ projects/calculator/src/app/calculator/calculator.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalcComponent } from './calc/calc.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CalcComponent
+  ],
   imports: [
     CommonModule
   ]
