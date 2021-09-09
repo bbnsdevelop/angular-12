6281567d92362ca7d8aaa11381a758b39de5e120
bbnsdevelop <bbnsdevelop@gmail.com>
2021-09-09 15:21:04 -0300

    implement methods in component

--- projects/calculator/src/app/calculator/calc/calc.component.ts
+++ projects/calculator/src/app/calculator/calc/calc.component.ts
@@ -8,20 +8,83 @@ import { CalcService } from '../services/calc.service';
 })
 export class CalcComponent implements OnInit {
 
-  result: number | undefined;
+  private result: number;
+  private num1: number;
+  private num2: number;
+  private operation: string;
 
   constructor(private service: CalcService) { }
 
   ngOnInit(): void {
-    this.calc();
+    this.clean();
   }
 
-  calc(){
-    let num1: number = 10;
-    let num2: number = 15;
-    let operation: string = '+'
-    this.result = this.service.calculate(num1, num2, operation);
+  clean(): void {
+    this.num1 = 0;
+    this.num2 = null;
+    this.operation = null;
+    this.result = null;
   }
 
+
+  addNumber(numberToCalc: string): void {
+  	if (this.operation === null || this.operation === undefined) {
+  	  this.num1 = Number(this.concatNumber(String(this.num1), numberToCalc));
+  	} else {
+      this.num2 = Number(this.concatNumber(String(this.num2), numberToCalc));
+  	}
+  }
+
+  concatNumber(numCurrent: string, numConcat: string): string {
+    if (numCurrent === '0' || numCurrent === null || isNaN(Number(numCurrent))) {
+  	  numCurrent = '';
+  	}
+
+  	if (numConcat === '.' && numCurrent === '') {
+  	  return '0.';
+  	}
+
+  	if (numConcat === '.' && numCurrent.indexOf('.') > -1) {
+  	  return numCurrent;
+  	}
+
+  	return numCurrent + numConcat;
+  }
+
+  defineOperation(operation: string): void {
+   
+  	if (this.operation === null || this.operation === undefined) {
+      this.operation = operation;
+      return;
+  	}
+
+   
+  	if (this.num2) {
+  		this.result = this.service.calculate(this.num1, this.num2, this.operation);
+  		this.operation = operation;
+  		this.num1 = this.result;
+  		this.num2 = undefined;
+  		this.result = undefined;
+  	}
+  }
+
+  calc(): void {
+  	if (this.num2 === null) {
+  		return;
+  	}
+
+  	this.result = this.service.calculate(this.num1, this.num2, this.operation);
+  }
+
+
+  get display(): string {
+  	if (this.result) {
+  		return this.result.toString();
+  	}
+  	if (this.num2) {
+  		return String(this.num2);
+  	}
+  	return String(this.num1);
+  }
 
 }
