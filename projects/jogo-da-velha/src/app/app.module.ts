f3b2586a11faa472dd919aeeecb93cc3a2a86c56
bbnsdevelop <bbnsdevelop@gmail.com>
2022-02-07 22:21:16 -0300

    crianto app jogo da velha

---
+++ projects/jogo-da-velha/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { JogoModule } from './jogo';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    JogoModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
